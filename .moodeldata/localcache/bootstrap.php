97080699146a7186945c769129c8ba738a65cc28
vctanaka <victor_tanaka@hotmail.com>
2024-06-10 17:10:11 -0400

    init

---
+++ .moodeldata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'jIXlIwDWEYXPsGXk38KztC1FsBbFPVCZ192.168.15.23';
+$CFG->bootstraphash = 'c3d96df01618fcc530dd2295505c6d2f';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
